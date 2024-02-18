2be36f5c462ac5b62e3e4f3679cb16ba0a5d2768
Josiah Parry <josiah.parry@gmail.com>
2024-02-17 19:37:32 -0500

    miscellaneous updates

--- r/R/aggregate.R
+++ r/R/aggregate.R
@@ -15,7 +15,7 @@
 #' y <- st_transform(st_read(yfp, quiet = TRUE), 27700)[2:3, ]
 #'
 #' matches <- rnet_match(x, y, dist_tolerance = 10, angle_tolerance = 5)
-#' rnet_aggregate_intensive(x, y, matches, value)
+ #' rnet_aggregate_intensive(x, y, matches, value)
 #' rnet_aggregate_extensive(x, y, matches, value)
 #' @rdname aggregate
 rnet_aggregate_extensive <- function(
@@ -80,3 +80,61 @@ rnet_aggregate_intensive <- function(
     ))
 }
 
+
+
+#' @param source data.frame that contains attributes that will be attributed to the target dataset
+#' @param matches results from `rnet_match()`. Contain `i`, `j`, and `shared_len`.
+#' @param extensive_vars character vector of variables names in `source` that will be aggregated to the target as an extensive variable. See Details.
+#' @param intensive_vars character vector of variable names in `source` that will be aggregated to hte target as intensive variables. See Details.
+#' @param source_len default `sf::st_length(source)`. The length of linestrings in the source data set. Used for weighted extensive variables.
+#' @param target_len the lengths of each line in the target dataset. Used for intensive variables.
+#' @export
+rnet_aggregate <- function(
+    source, matches, extensive_vars = NULL, intensive_vars = NULL,
+    source_len = sf::st_length(source),
+    target_len = NULL
+) {
+
+  # target_len cannot be missing when intensive_vars is not null
+  # source_len cannot be missing when extensive_vars is not null
+
+  # create column names with suffixes to be used
+  ext_nms <- paste0(extensive_vars, "_ext")
+  int_nms <- paste0(intensive_vars, "_int")
+
+  # fetch neighbor index
+  j <- matches$j
+
+  # get the neighboring values for each of the intensive and extensive vars
+  # we give them names of the columns with a suffix based on whether or
+  # not it is an extensive or intensive variable
+  ij <- rlang::set_names(
+    lapply(
+      c(extensive_vars, intensive_vars),
+      function(.x) y[[.x]][j]
+    ),
+    c(ext_nms, int_nms)
+  )
+
+  # combine the `ij` values to the original matches
+  dplyr::bind_cols(matches, ij) |>
+    dplyr::mutate(
+      # calculate weight for intensive variables
+      wt_int = shared_len / as.numeric(x_len[i]),
+      # calulate weight for extensive variables
+      wt_ext = shared_len / as.numeric(y_len[j])
+    ) |>
+    dplyr::group_by(i) |>
+    dplyr::summarise(
+      # handle extensive variables
+      dplyr::across(
+        dplyr::all_of(ext_nms),
+        ~ sum(.x, wt_ext, na.rm = TRUE)
+      ),
+      # handle intensive variables
+      dplyr::across(
+        dplyr::all_of(int_nms),
+        ~ weighted.mean(.x, wt_int, na.rm = TRUE)
+      )
+    )
+}
